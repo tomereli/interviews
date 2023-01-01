c348a7ec5e07f8e7488985546ee347191a9eb990
Tomer Arbel Eliyahu <tomereliyahu1@gmail.com>
2023-01-01 17:43:06 +0200

    Udemy - Linked List - the reset
    Signed-off-by: Tomer Arbel Eliyahu <tomereliyahu1@gmail.com>

---
+++ Udemy/dsa-python/EXERCISE-LL-Pop.py
@@ -0,0 +1,65 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self) -> Node:
+        node = self.head
+        if self.length == 0:
+            return None
+        if self.length == 1:
+            self.tail = self.head = None
+        else:
+            node = self.tail
+            ptr = self.head
+            while ptr.next != self.tail:
+                ptr = ptr.next
+            ptr.next = None
+            self.tail = ptr
+        self.length -= 1
+        return node
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+
+# (2) Items - Returns 2 Node
+print(my_linked_list.pop().value)
+# (1) Item -  Returns 1 Node
+print(my_linked_list.pop().value)
+# (0) Items - Returns None
+print(my_linked_list.pop())
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    2
+    1
+    None
+
+"""
